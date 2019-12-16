63150cb6ce37dff3437b2a322a3968520663ce9e
Your Name <buitiendo@sun-asterisk.com>
2019-12-16 07:54:57 +0700

    updating-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.all.page(params[:page]).per(5)
+  end
+
   def show
     @user = User.find params[:id]
   end
@@ -18,9 +26,45 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find(params[:id]) 
+    if @user.update_attributes user_param 
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find params[:id]
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :city_id)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please login"
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
