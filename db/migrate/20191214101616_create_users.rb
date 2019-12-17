75f42f7d17db0469309458c2bd024b7f1984b678
DoBT <bui.tien.do@sun-asterisk.com>
2019-12-17 14:48:16 +0700

    radio_buttons

--- db/migrate/20191214101616_create_users.rb
+++ db/migrate/20191214101616_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
+      t.integer :gender
       t.string :email
       t.references :city, foreign_key: true
 
